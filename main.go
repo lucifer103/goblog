0b54dab5540f4e168aa548aa8e3deb739521f178
翟宇鑫 <zhaiyuxin103@hotmail.com>
2021-01-01 21:02:22 +0800

    ✨feat: 404 状态码

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	} else if r.URL.Path == "/about" {
 		fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或者建议，请联系 "+"<a href=\"mailto:zhaiyuxin103@hotmail.com\">zhaiyuin103@hotmail.com</a>")
 	} else {
+		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+"<p>如有疑惑，请联系我们。</p>")
 	}
 }
