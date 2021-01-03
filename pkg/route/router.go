1977a6e39ae1461229d068934dd7018ac22afd77
翟宇鑫 <zhaiyuxin103@hotmail.com>
2021-01-03 14:10:00 +0800

    🦄refactor: 重构至 PagesController

--- pkg/route/router.go
+++ pkg/route/router.go
@@ -3,6 +3,8 @@ package route
 import (
 	"net/http"
 
+	"goblog/routes"
+
 	"github.com/gorilla/mux"
 )
 
@@ -12,6 +14,7 @@ var Router *mux.Router
 // Initialize 初始化路由
 func Initialize() {
 	Router = mux.NewRouter()
+	routes.RegisterWebRoutes(Router)
 }
 
 // Name2URL 通过路由名称来获取 URL
