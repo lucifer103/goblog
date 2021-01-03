5b3aea187fd6f05dd80f010603b83a8a436b1a48
翟宇鑫 <zhaiyuxin103@hotmail.com>
2021-01-03 13:32:52 +0800

    🦄refactor: 重构  initDB 和 createTables

--- main.go
+++ main.go
@@ -8,12 +8,11 @@ import (
 	"net/url"
 	"strconv"
 	"strings"
-	"time"
 	"unicode/utf8"
 
-	"github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 
+	"goblog/pkg/database"
 	"goblog/pkg/logger"
 	"goblog/pkg/route"
 	"goblog/pkg/types"
@@ -21,43 +20,6 @@ import (
 
 var router *mux.Router
 var db *sql.DB
-
-func initDB() {
-	var err error
-	config := mysql.Config{
-		User:                 "root",
-		Passwd:               "root",
-		Addr:                 "localhost:3306",
-		Net:                  "tcp",
-		DBName:               "goblog",
-		AllowNativePasswords: true,
-	}
-
-	// 准备数据库连接池
-	db, err = sql.Open("mysql", config.FormatDSN())
-	logger.LogError(err)
-	// 设置最大连接数
-	db.SetMaxOpenConns(100)
-	// 设置最大空闲连接数
-	db.SetMaxIdleConns(25)
-	// 设置每个连接的过期时间
-	db.SetConnMaxLifetime(5 * time.Minute)
-
-	// 尝试连接，失败会报错
-	err = db.Ping()
-	logger.LogError(err)
-}
-
-func createTables() {
-	createArticlesSQL := `CREATE TABLE IF NOT EXISTS articles(
-		id bigint(20) PRIMARY KEY AUTO_INCREMENT NOT NULL,
-		title varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-		body longtext COLLATE utf8mb4_unicode_ci
-	); `
-
-	_, err := db.Exec(createArticlesSQL)
-	logger.LogError(err)
-}
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Hello，欢迎来到 goblog！</h1>")
@@ -445,8 +407,9 @@ func removeTrailingSlash(next http.Handler) http.Handler {
 }
 
 func main() {
-	initDB()
-	createTables()
+
+	database.Initialize()
+	db = database.DB
 
 	route.Initialize()
 	router = route.Router
