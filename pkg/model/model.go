5091483b6974e4bc1956d02537623d60557f2ccf
翟宇鑫 <zhaiyuxin103@hotmail.com>
2021-01-03 18:31:47 +0800

    ✨feat: 模型基类和 view 包

--- pkg/model/model.go
+++ pkg/model/model.go
@@ -3,11 +3,22 @@ package model
 import (
 	// GORM 的 Mysql 数据库驱动导入
 	"goblog/pkg/logger"
+	"goblog/pkg/types"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	gormlogger "gorm.io/gorm/logger"
 )
+
+// BaseModel 模型基类
+type BaseModel struct {
+	ID uint64
+}
+
+// GetStringID 获取 ID 的字符串格式
+func (a BaseModel) GetStringID() string {
+	return types.Uint64ToString(a.ID)
+}
 
 // DB gorm.DB 对象
 var DB *gorm.DB
