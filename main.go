d233250bd3bd8d698d87fc28d8680c107792c5cf
翟宇鑫 <zhaiyuxin103@hotmail.com>
2021-01-03 11:46:25 +0800

    ✨feat: 完成第一个测试

--- main.go
+++ main.go
@@ -35,7 +35,7 @@ func initDB() {
 	db, err = sql.Open("mysql", config.FormatDSN())
 	checkError(err)
 	// 设置最大连接数
-	db.SetMaxOpenConns(25)
+	db.SetMaxOpenConns(100)
 	// 设置最大空闲连接数
 	db.SetMaxIdleConns(25)
 	// 设置每个连接的过期时间
