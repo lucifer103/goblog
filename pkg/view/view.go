b96c4b128ea5ff8856b30af87fccc99acf2f351f
翟宇鑫 <zhaiyuxin103@hotmail.com>
2021-01-16 16:16:08 +0800

    ✨feat: 注册页面

--- pkg/view/view.go
+++ pkg/view/view.go
@@ -9,12 +9,25 @@ import (
 	"text/template"
 )
 
-// Render 渲染视图
+// D 是 map[string]interface{} 的简写
+type D map[string]interface{}
+
+// Render 渲染通用视图
 func Render(w io.Writer, data interface{}, tplFiles ...string) {
+	RenderTemplate(w, "app", data, tplFiles...)
+}
+
+// RenderSimple 渲染简单的视图
+func RenderSimple(w io.Writer, data interface{}, tplFiles ...string) {
+	RenderTemplate(w, "simple", data, tplFiles...)
+}
+
+// RenderTemplate 渲染视图
+func RenderTemplate(w io.Writer, name string, data interface{}, tplFiles ...string) {
 	// 1. 设置模板相对路径
 	viewDir := "resources/views/"
 
-	// 2. 语法糖，将 articles.show 更正为 articles/show
+	// 2. 遍历传参文件列表 Slice，设置正确的路径，支持 dir.filename 的语法糖
 	for i, f := range tplFiles {
 		tplFiles[i] = viewDir + strings.Replace(f, ".", "/", -1) + ".gohtml"
 	}
@@ -34,5 +47,5 @@ func Render(w io.Writer, data interface{}, tplFiles ...string) {
 	logger.LogError(err)
 
 	// 6. 渲染模板
-	tmpl.ExecuteTemplate(w, "app", data)
+	tmpl.ExecuteTemplate(w, name, data)
 }
