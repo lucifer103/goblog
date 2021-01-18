7aef9921c0378a57516d7cfb12afe0fca424b340
翟宇鑫 <zhaiyuxin103@hotmail.com>
2021-01-18 17:30:26 +0800

    🚨fix: removing linter warnings

--- app/models/article/article.go
+++ app/models/article/article.go
@@ -12,7 +12,7 @@ type Article struct {
 
 	Title      string `gorm:"type:varchar(255);not null;" valid:"title"`
 	Body       string `gorm:"type:longtext;not null;" valid:"body"`
-	UserID     uint64 `gorm:"not null;index`
+	UserID     uint64 `gorm:"not null;index"`
 	User       user.User
 	CategoryID uint64 `gorm:"not null;default:4;index"`
 }
